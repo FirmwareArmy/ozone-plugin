262144557b5aab002868e667ff6bbd0fca581e8d
Sébastien Besombes <sebastien.besombes@gmail.com>
2020-03-29 23:10:32 +0200

    Added automatic project opening for recognized targets

--- ozone_plugin/build_commands/debug.py
+++ ozone_plugin/build_commands/debug.py
@@ -10,6 +10,8 @@ from log import log
 import dependency
 import re
 import subprocess 
+from debugtools import print_stack
+import tornado.template as template
 
 def to_relative_path(path):
     home = os.path.expanduser("~")
@@ -90,11 +92,64 @@ def cmake_get_variable(path, name):
                 line = file.readline()
             
     except Exception as e:
+        print_stack()
         log.error(f"{e}")
         exit(1)
     
     return None
+
+def add_project_file(arch, target):
+    project_load = [
+        f'Project.AddPathSubstitute ("{os.path.abspath(os.getcwd())}", "$(ProjectDir)");',
+    ]
+
+    definition = os.path.join(arch['path'], arch['module'], arch['definition'])
+    device = cmake_get_variable(definition, "DEVICE")
+    cpu = cmake_get_variable(definition, "CPU")
+
+    cpu_map = {}
+    cpu_svd = {}
+    cpu_freertos = {}
+    device_svd = {}
+
+    cpu_map['cortex-m0plus'] = "Cortex-M0+"
+    cpu_svd['cortex-m0plus'] = "Cortex-M0"
+    cpu_freertos['cortex-m0plus'] = "CM0"
+    device_svd['samd21g18au'] = "ATSAMD51G18A"
     
+    if cpu not in cpu_map:
+        low.warning(f"No correspondance defined for device {cpu}")
+        return False
+    
+    if device not in device_svd:
+        low.warning(f"No correspondance defined for device {device}")
+        return False
+    
+    project_load.append(f'Project.SetDevice ("{cpu_map[cpu]}");') 
+    project_load.append('Project.SetHostIF ("USB", "");')
+    project_load.append('Project.SetTargetIF ("SWD");')
+    project_load.append('Project.SetTIFSpeed ("12 MHz");')
+
+    project_load.append(f'Project.AddSvdFile ("{os.path.abspath(plugin_path)}/ozone/Config/CPU/{cpu_svd[cpu]}.svd");')
+    project_load.append(f'Project.AddSvdFile ("{os.path.abspath(plugin_path)}/ozone/Config/Peripherals/{device_svd[device]}.svd");')
+    project_load.append(f'Project.SetOSPlugin("FreeRTOSPlugin_{cpu_freertos[cpu]}");')
+    project_load.append(f'File.Open ("$(ProjectDir)/output/{target["name"]}/bin/firmware.elf");')
+    
+    # write CMakeLists.txt from template
+    try:
+        loader = template.Loader(os.path.join(plugin_path, 'template'), autoescape=None)
+        cmakelists = loader.load("project.jdebug").generate(
+            project_load="\n".join(project_load)
+        )
+        with open("project.jdebug", "w") as f:
+            f.write(cmakelists.decode("utf-8"))
+    except Exception as e:
+        print_stack()
+        log.error(f"{e}")
+        exit(1)
+
+    return True
+
 def project_debug(args, config, **kwargs):
     global plugin_path
 
@@ -105,8 +160,8 @@ def project_debug(args, config, **kwargs):
             log.error("Current path is not a project")
             exit(1)
     except Exception as e:
-        log.error(f"{e}")
         print_stack()
+        log.error(f"{e}")
         return
 
     # get target config
@@ -145,9 +200,18 @@ def project_debug(args, config, **kwargs):
         build_config = Config(None, os.path.join(build_path, 'army.toml'))
         build_config.load()
     except Exception as e:
-        log.error(f"{e}")
         print_stack()
+        log.error(f"{e}")
         return
+
+    # load dependencies
+    if build_config.config['build']['debug']:
+        dependencies = dependency.load_dev_dependencies(config, target)
+    else:
+        dependencies = dependency.load_dependencies(config, target)
+
+    # get device
+    arch = get_arch(config, target, dependencies)
 
     log.info("Debug $device with Ozone")
 # 
@@ -161,10 +225,19 @@ def project_debug(args, config, **kwargs):
         commandline = [
             f"{os.path.join(plugin_path, ozoneexe)}",
         ]
-        
+
+        # add CMakeLists.txt
+        if add_project_file(arch, target):
+            commandline += [
+                '-project', f'project.jdebug'
+            ]
+
+        commandline += ['&']
         log.info(" ".join(commandline))
-        subprocess.check_call(commandline)
+        #subprocess.check_call(commandline)
+        os.system(" ".join(commandline))        
     except Exception as e:
+        print_stack()
         log.error(f"{e}")
         exit(1)
         
